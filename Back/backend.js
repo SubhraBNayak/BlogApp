bfbc46b8d3927ecbc07eb61f86da78aabc07579b
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-08-27 20:34:41 +0530

    PHASE 1 (SIGNIN/SIGNUP) DONE!

--- Back/backend.js
+++ Back/backend.js
@@ -7,13 +7,20 @@ const validator = require('validator');
 const dns = require('dns');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const {UserModel} = require('./db');
+const cors = require('cors');
+const path = require('path');
+const { UserModel } = require('./db');
 
 const JWTSECRET = "developersOfBlogger"
 const app = express();
 mongoose.connect("mongodb+srv://subhrabikiran:LeZshcaShnL3DEnU@cluster0.je3gfax.mongodb.net/blog-app-database");
 
 /*-----------------MIDDLEWARES-----------------*/
+
+/*
+    cross-origin-resource-sharing (cors)
+*/
+app.use(cors());
 
 /*
     parsing middleware
@@ -25,7 +32,7 @@ app.use(express.json());
     @emailCheck middleware first validates the email syntax (validator) and checks the email domain (dns)
         status(400) -> bad request, wrong/invalid email address sent by the user
 */
-function emailCheck(req, res, next){
+function emailCheck(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
     if (validator.isEmail(email)) {
@@ -41,13 +48,12 @@ function emailCheck(req, res, next){
                 next();
             }
         })
-    }else{
+    } else {
         res.status(400).send({
-            message : "something wrong with the email."
+            message: "something wrong with the email."
         })
     }
 }
-
 
 /*-----------------ENDPOINTS FOR @BLOGGER-----------------*/
 
@@ -56,61 +62,79 @@ function emailCheck(req, res, next){
     @signup goes through the parsing and emailChecking middleware,
     hashes the password (bcrypt.hash) and stores it in the database
         status(500) -> internal server error, no/bad response from the database
+        status(409) -> user conflict, user email already exists
 */
-app.post("/signup", emailCheck, async function(req, res){
+app.post("/api/signup", emailCheck, async function (req, res) {
     const email = req.email;
     const password = req.password;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 5);
-        await UserModel.create({
-            email : email,
-            password : hashedPassword
-        })
-    } catch (error) {
-        res.status(500).send({
-            message : "bad response from database!"
-        })
-    }  
-    res.status(200).send({
-        message : "user signed up!"
+    const user = await UserModel.findOne({
+        email: email
     })
+    if (user) {
+        res.status(409).send({
+            message: "user already exists! try different email"
+        })
+    } else {
+        try {
+            const hashedPassword = await bcrypt.hash(password, 5);
+            await UserModel.create({
+                email: email,
+                password: hashedPassword
+            })
+        } catch (error) {
+            res.status(500).send({
+                message: "bad response from database!"
+            })
+        }
+        res.status(200).send({
+            message: "user signed up!"
+        })
+    }
 })
 
 /*
     (OK TESTED)
     @signin goes through emailCheck middleware,
-    hashes the password and compares tha hashed password (bcrypt.compare) with the password in the db.
+    hashes the password and compares the hashed password (bcrypt.compare) with the password in the db.
         status(404) -> request has not been applied as resource not found (email must have been entered incorrectly by user)
         status(200) -> all ok, user signed in, credentials verified
         status(401) -> invalid credentials, entered password is wrong
 */
-app.post("/signin", emailCheck, async function(req, res){
+app.post("/api/signin", emailCheck, async function (req, res) {
     const email = req.email;
     const password = req.password;
     const user = await UserModel.findOne({
-        email : email
+        email: email
     })
     if (user) {
         const hashedPassword = user.password;
         const passwordVerified = await bcrypt.compare(password, hashedPassword);
         if (passwordVerified) {
             const token = jwt.sign({
-                email : email
+                email: email
             }, JWTSECRET)
             user.online = true;
-            res.status(200).send({
-                token : token
+            res.status(200).json({
+                token: token
             })
-        }else{
+        } else {
             res.status(401).send({
-                message : "wrong password"
+                message: "wrong password"
             })
         }
-    }else{
+    } else {
         res.status(404).send({
-            message : "user doesn't exist!"
+            message: "user doesn't exist!"
         })
     }
 })
 
-app.listen(3000);
+/*
+    serving static files
+*/
+app.use(express.static(path.join(__dirname, "Front")));
+
+//http://127.0.0.1:5000/Front/signin.html to open the frontend
+app.listen(3000, () => {
+    console.log("server running at http://localhost:3000")
+});
--- Front/signinScript.js
+++ Front/signinScript.js
@@ -1,58 +1,66 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.7/axios.min.js"
-
 /*
     signup() function takes the email and password as the input,
     and creates a new dictionary in the users collection of blog-app-database.
 */
-async function signup(){
+async function signup() {
     const email = document.getElementById("signup-email").value
     const password = document.getElementById("signup-password").value
     const confirmpassword = document.getElementById("signup-confirmpassword").value
 
     if (email && password && confirmpassword) {
         if (password == confirmpassword) {
-            const response = await axios.post("/signup", {
-                email : email,
-                password : password
-            })
-            if (response.status == 200) {
-                alert("sign-up successful");
-            }else if(response.status == 400){
-                alert("bad request, wrong/invalid email address sent by the user!");
-            }else if(response.status == 500){
-                alert("internal server error, no/bad response from the database");
-            }else{
-                alert("unknown error occured!");
+            try {
+                const response = await axios.post("http://localhost:3000/api/signup", {
+                    email: email,
+                    password: password
+                })
+                if (response.status == 200) {
+                    alert("sign-up successful");
+                }
+            } catch (error) {
+                if (error.response) {
+                    if (error.response.status == 400) {
+                        alert("bad request, wrong/invalid email address sent by the user!");
+                    } else if (error.response.status == 500) {
+                        alert("internal server error, no/bad response from the database");
+                    } else if (error.response.status == 409) {
+                        alert("user already exists! try logging in");
+                    } else {
+                        alert("unknown error occured!");
+                    }
+                } else {
+                    alert("network error or server unrea    chable!");
+                }
             }
-        }else{
+        } else {
             alert("confirm password doesn't match!");
             document.getElementById("signup-confirmpassword").value = "";
         }
-    }else{
+    } else {
         alert("email or password not provided")
     }
 }
 
-async function signin(){
+async function signin() {
     const email = document.getElementById("signin-email").value
     const password = document.getElementById("signin-password").value
 
     if (email && password) {
-        const response = await axios.post("/signin", {
-            email : email,
-            password : password
+        const response = await axios.post("http://localhost:3000/api/signin", {
+            email: email,
+            password: password
         })
         if (response.status == 200) {
             localStorage.setItem("token", response.data.token)
             alert("signin successful!Redirecting...")
-        }else if(response.status == 404){
+        } else if (response.status == 404) {
             alert("user doesn't exist! incorrect email")
-        }else if(response.status == 401){
+        } else if (response.status == 401) {
             alert("wrong password! authentication failed")
-        }else{
+        } else {
             alert("unknown error occured")
         }
-    }else{
+    } else {
         alert("email or password not provided")
     }
 }
